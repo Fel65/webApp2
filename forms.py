d967665615ae28290acee3c9cdd5ad8ce23bb380
Fel65 <93129277+Fel65@users.noreply.github.com>
2022-02-22 20:04:17 +0000

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, ValidationError
+from application.models import Chore
+
+class ChoreForm(FlaskForm):
+    
+    #id = db.Column(db.Integer, primary_key=True)
+    charge = StringField('Enter the charge')
+    job_done = StringField('Is it complete')
+    house_id = StringField('House details(*)', validators=[DataRequired()])
+    chore_description = StringField("Chore Description", validators=[DataRequired()])
+    submit = SubmitField("Add Chore")
+    #CRUD Functionality is excuted here
+    def validate_task(self, chore):
+        chore = Chore.query.all()
+        for chore in chores:
+            if chore.task == chore.data:
+                raise ValidationError('You already added this chore')
+
+job_status =[
+            ("complete", "Completed"),
+            ("id", "Recent"),
+            ("old", "Old"),
+            ('incomplete', "Incomplete")
+        ]
+
+#
---
+++ routes.py
@@ -0,0 +1,29 @@
+from application import app, db
+from application.models import Chore
+from flask import render_template, request,url_for, redirect
+from application.forms import ChoreForm
+
+@app.route('/')
+def home():
+    all_chores = Choress.query.all()
+    return render_template("index.html", all_chores = all_chores, title="Home")
+
+
+@app.route('/create_chore', methods= ["GET","POST"])
+def create_chore():
+    form = ChoreForm()
+    
+    if request.method == "POST":
+    
+        new_chore = Chores(
+            description=form.description.data,
+            charge=form.description.data, 
+            address=form.description.data,
+            Job_done=form.description )
+        
+        db.session.add(new_chore)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("create_chore.html", title="Add a Chore", form=form)
+
+
